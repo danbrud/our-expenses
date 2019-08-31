5d50662e9007429648f0547fe4403b25df5a407e
Danny <dannybrudner@gmail.com>
2019-08-31 13:17:30 +0300

    created simple handle input function with general input variable

--- src/stores/GeneralStore.js
+++ src/stores/GeneralStore.js
@@ -2,5 +2,9 @@ import { observable, action, computed } from "mobx";
 
 
 export class GeneralStore {
-    
+    @observable input = ""
+
+    @action handleInput(e) {
+        this[e.target.name] = e.target.value
+    }
 }
