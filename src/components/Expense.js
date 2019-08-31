e2bd72308b37f820d155cc5377503e55ea80328a
Danny <dannybrudner@gmail.com>
2019-08-31 17:29:00 +0300

    basic grid styling in expense component

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import '../styles/Expense.css'
 
 export default function Expense(props) {
 
     return (
-        <div>
-            {props.expense.user}
+        <div id="expense">
+            <div>{props.expense.user}</div>
+            <div>{props.expense.name}</div>
+            <div>{props.expense.amount}</div>
+            <div>{props.expense.category}</div>
+            <div>{props.expense.date.toString()}</div>
         </div>
     )
 }
