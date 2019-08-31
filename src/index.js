84a557056eb2ac2142f6e3fd3ebe91da82bdbe30
Danny <dannybrudner@gmail.com>
2019-08-31 13:21:23 +0300

    added provider to index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Expenses } from './stores/Expenses';
+import { GeneralStore } from './stores/GeneralStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const expensesStore = new Expenses()
+const generalStore = new GeneralStore()
+const stores = { expensesStore, generalStore }
+
+ReactDOM.render(<Provider {...stores}> <App /> </Provider>, document.getElementById('root'));
+
 serviceWorker.unregister();
