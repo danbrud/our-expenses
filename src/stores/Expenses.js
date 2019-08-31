777c31574eb8b3168bdc665d4d96c13502c239c9
Danny <dannybrudner@gmail.com>
2019-08-31 12:58:44 +0300

    added expenses observable and initial dummy data

--- src/stores/Expenses.js
+++ src/stores/Expenses.js
@@ -2,5 +2,12 @@ import { observable, action, computed } from "mobx";
 
 
 export class Expenses {
-    
+    @observable expenses = [
+        {_id: "7897987", user: "Danny", amount: 20, date: new Date(), category: "Apt"},
+        {_id: "jhksd", user: "Danny", amount: 67, date: new Date(), category: "Apt"},
+        {_id: "l2j1", user: "Tal", amount: 90, date: new Date(), category: "Food"},
+        {_id: "lk;l2k312", user: "Tal", amount: 10, date: new Date(), category: "Groceries"},
+        {_id: "798hkjhkdsdsa", user: "Tal", amount: 210, date: new Date(), category: "Groceries"},
+        {_id: "787wdjshdashdsa76", user: "Danny", amount: 10, date: new Date(), category: "Eating out"}
+    ]
 }
