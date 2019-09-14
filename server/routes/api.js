3515c3516085fb84d52734469adeec9835f4761e
Danny <dannybrudner@gmail.com>
2019-09-14 15:51:39 +0300

    added get by date on backend

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,7 +9,20 @@ router.get('/sanity', function (req, res) {
 })
 
 router.get('/expenses', async function(req, res) {
-    const expenses = await Expense.find({})
+    let date, currentMonth = req.query.month, minDate, maxDate 
+    
+    if(currentMonth) {
+        date = new Date()
+        currentMonth = parseInt(currentMonth)
+        minDate = new Date(date.getFullYear(), currentMonth, 2)
+        maxDate = new Date(date.getFullYear(), currentMonth + 1, 0)
+        maxDate.setDate(maxDate.getDate() + 1)
+    }
+    
+    const expenses = currentMonth 
+        ? await Expense.find({ date: { $gte: minDate, $lte: maxDate }}) 
+        : await Expense.find({})
+
     res.send(expenses)
 })
 
