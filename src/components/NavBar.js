7acd63967fef9c9dec47f4e04f35c6ab4a2db2aa
danbrud <dannybrudner@users.noreply.github.com>
2019-10-04 17:58:46 +0300

    added sidebar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,22 +27,66 @@ const useStyles = makeStyles(theme => ({
   },
   bar: {
     backgroundColor: '#34495e',
-  }
-}));
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+}))
 
 export default function NavBar(props) {
-  const classes = useStyles();
+  const classes = useStyles()
+  const [state, setState] = React.useState({
+    left: false,
+  })
+
+  const toggleDrawer = (side, open) => event => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return
+    }
+
+    setState({ ...state, [side]: open })
+  }
+
   const sum = props.store.sumCurrentMonth.toString()
     .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 
 
-  console.log(window.location)
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {['הוצאות', 'הוסף הוצאה', 'סיכום הוצאות'].map((text, index) => (
+          <Link to={text === 'הוצאות' ? '/' : text === 'הוסף הוצאה' ? '/add-expense' : '/reports'}>
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </div>
+  )
+
   return (
     <div className={classes.root}>
       <AppBar className={classes.bar} position="fixed">
+        <SwipeableDrawer
+          open={state.left}
+          onClose={toggleDrawer('left', false)}
+          onOpen={toggleDrawer('left', true)}
+        >
+          {sideList('left')}
+        </SwipeableDrawer>
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+            <MenuIcon onClick={toggleDrawer('left', true)} />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
 
@@ -42,7 +94,7 @@ export default function NavBar(props) {
           {
             window.location.pathname === '/'
               ? <Typography variant="h6">
-                  {sum} :סה"כ הוצאות
+                {sum} :סה"כ הוצאות
                 </Typography>
               : null
           }
