0688a6374934d34d15dd27273527a298974d74c3
Danny <dannybrudner@gmail.com>
2019-10-20 17:46:21 +0300

    removed Provider from index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'mobx-react'
-import { Expenses } from './stores/Expenses';
-import { GeneralStore } from './stores/GeneralStore';
 
 
-const expensesStore = new Expenses()
-const generalStore = new GeneralStore()
-const stores = { expensesStore, generalStore }
-
-ReactDOM.render(<Provider {...stores}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register();
