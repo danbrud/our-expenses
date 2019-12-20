50a43247134a47f79fad7ac2408b47984ce29f8c
danbrud <dannybrudner@users.noreply.github.com>
2019-12-20 10:55:38 +0200

    added unified topology to db connection

--- src/utils.js
+++ src/utils.js
@@ -19,15 +19,15 @@ export const months = [
 ]
 
 export const colors = [
-    '#D4F2D2', 
-    '#BEB7DF', 
-    '#FDE8E9', 
-    '#70E4EF', 
-    '#DEF4C6', 
-    '#73E2A7', 
-    '#C3DFE0', 
-    '#4BA3C3', 
-    '#9C9990', 
+    '#D4F2D2',
+    '#BEB7DF',
+    '#FDE8E9',
+    '#70E4EF',
+    '#DEF4C6',
+    '#73E2A7',
+    '#C3DFE0',
+    '#4BA3C3',
+    '#9C9990',
     '#CFD2B2',
     '#F78764',
     '#D7F9F1',
@@ -36,4 +36,4 @@ export const colors = [
 
 export const users = ['טל', 'דני']
 
-export const API_URL = ''//'http://localhost:4000'
+export const API_URL = 'http://localhost:4000'
