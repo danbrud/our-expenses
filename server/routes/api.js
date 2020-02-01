ddefea97839273cac6d3959f559f0223e04703d3
Danny Brudner <dannybrudner@gmail.com>
2020-02-01 14:57:55 +0200

    added account functionality server side

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const getExpensesByDate = require('../utils/utilFunctions').getExpensesByDate
 
+const Account = require('../models/Account')
 const Expense = require('../models/Expense')
 
 
@@ -9,7 +10,7 @@ router.get('/sanity', function (req, res) {
     res.send('OK!')
 })
 
-router.get('/expenses', async function(req, res) {
+router.get('/expenses/:accountId', async function(req, res) {
     let minDate, maxDate, currentDate = req.query.date
 
     if(currentDate) {
@@ -19,23 +20,49 @@ router.get('/expenses', async function(req, res) {
         maxDate.setDate(maxDate.getDate() + 1)
     }
 
+    const accountId = req.params.accountId
     const expenses = currentDate
-        ? await getExpensesByDate(minDate, maxDate)
-        : await Expense.find({})
+        ? await getExpensesByDate(minDate, maxDate, accountId)
+        : await Expense.find({ accountId: accountId })
 
     res.send(expenses)
 })
 
 router.post('/expense', async function(req, res) {
     const expense = new Expense(req.body)
-    const savedExpense = await expense.save()
-
-    res.send(savedExpense)
+    await expense.save()
+    await Account.findOneAndUpdate({ _id: expense.accountId }, { $push: { expenses: expense._id }})
+    
+    res.send(expense)
 })
 
 router.delete('/expenses/:id', async function(req, res) {
     const expense = await Expense.findOneAndDelete({_id: req.params.id})
+    await Account.findOneAndUpdate({ _id: expense.accountId }, { $pull: { expenses: expense._id }})
+
     res.send(expense)
+})
+
+router.post('/accounts', async function(req, res) {
+    const account = new Account(req.body)
+    await account.save()
+    res.send(account)
+})
+
+router.get('/accounts/:username', async function(req, res) {
+    // Account.findOne({ username: req.params.username }).populate('expenses').exec(function(err, account) {
+    //     res.send(account)
+    // })
+
+    const account = await Account.find({ username: req.params.username })
+    res.send(account)
+})
+
+router.put('/accounts', async function(req, res) {
+    const { fieldToUpdate, accountId, data } = req.body
+    const account = await Account.findOneAndUpdate({ _id: accountId }, { $push: { [fieldToUpdate]: data } }, { new: true }) 
+
+    res.send(account)
 })
 
 module.exports = router
--- server/utils/utilFunctions.js
+++ server/utils/utilFunctions.js
@@ -1,7 +1,7 @@
 const Expense = require('../models/Expense')
 
-const getExpensesByDate = async function (minDate, maxDate) {
-  const expenses = await Expense.find({})
+const getExpensesByDate = async function (minDate, maxDate, accountId) {
+  const expenses = await Expense.find({ accountId: accountId })
   return expenses
     .filter(e => e.date >= minDate && e.date < maxDate)
     .sort((a, b) => a.date - b.date)
