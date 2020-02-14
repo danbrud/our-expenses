47424222a7b2f66056947a2d6804e73d4224ab38
Danny Brudner <dannybrudner@gmail.com>
2020-02-14 17:21:35 +0200

    updated validators for correct naming

--- server/utils/utilFunctions.js
+++ server/utils/utilFunctions.js
@@ -10,16 +10,16 @@ const getExpensesByDate = async function (minDate, maxDate, accountId) {
     .sort((a, b) => a.date - b.date)
 }
 
-function validateLoginInput(data) {
+const validateLoginInput = function(data) {
   let errors = {};
   // Convert empty fields to an empty string so we can use validator functions
-  data.email = !isEmpty(data.email) ? data.email : "";
+  data.username = !isEmpty(data.username) ? data.username : "";
   data.password = !isEmpty(data.password) ? data.password : "";
   // Email checks
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
+  if (Validator.isEmpty(data.username)) {
+    errors.username = "Email field is required";
+  } else if (!Validator.isEmail(data.username)) {
+    errors.username = "Email is invalid";
   }
   // Password checks
   if (Validator.isEmpty(data.password)) {
@@ -31,22 +31,18 @@ function validateLoginInput(data) {
   };
 };
 
-function validateRegisterInput(data) {
+const validateRegisterInput = function(data) {
   let errors = {};
   // Convert empty fields to an empty string so we can use validator functions
-  data.name = !isEmpty(data.name) ? data.name : "";
-  data.email = !isEmpty(data.email) ? data.email : "";
+  data.username = !isEmpty(data.username) ? data.username : "";
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
-  // Name checks
-  if (Validator.isEmpty(data.name)) {
-    errors.name = "Name field is required";
-  }
+  
   // Email checks
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
+  if (Validator.isEmpty(data.username)) {
+    errors.username = "Email field is required";
+  } else if (!Validator.isEmail(data.username)) {
+    errors.username = "Email is invalid";
   }
   // Password checks
   if (Validator.isEmpty(data.password)) {
