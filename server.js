b12a9d581a1b9b4a7e2951372c5d3b793244dd87
danbrud <dannybrudner@users.noreply.github.com>
2020-05-13 21:24:59 +0300

    fixed cors issue

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/our-expenses", 
 })
 
 //CORS
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV !== 'production') {
     app.use(function (req, res, next) {
         res.header('Access-Control-Allow-Origin', '*')
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
