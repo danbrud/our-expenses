841eb671f9d33c78ce2501025fcfe66b03843ea6
danbrud <dannybrudner@users.noreply.github.com>
2020-05-15 15:58:57 +0300

    create initial expense data

--- src/context/Expenses.js
+++ src/context/Expenses.js
@@ -1,11 +1,23 @@
-class Expenses {
+import { createContext, useContext } from 'react'
+
+class ExpenseData {
   constructor() {
     this.expenses = []
   }
-
 
   async getExpenses(shouldGetByDate = true) {
     const optionalParam = shouldGetByDate ? `?date=${currentDate}` : ''
     const res = await axios.get(`${API_URL}/api/expenses/${currentAccount._id}${optionalParam}`)
     this.expenses = res.data
+  }
 }
+
+export const expenseData = new ExpenseData()
+
+const ExpensesContext = createContext({})
+
+export const ExpensesProvider = ExpensesContext.Provider
+
+export const ExpensesConsumer = ExpensesContext.Consumer
+
+export const useExpensesStore = () => useContext(ExpensesContext)
