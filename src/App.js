b0e6cb40101d3b40bf17247d837960cdcb7a5eab
danbrud <dannybrudner@users.noreply.github.com>
2020-05-15 12:17:37 +0300

    initial income component

--- src/App.js
+++ src/App.js
@@ -59,10 +59,10 @@ function App(props) {
     <Router>
       {props.auth.authenticated ? <NavBar expenses={expenses} logoutUser={logoutUser} /> : null }
       <Switch>
-        <ProtectedRoute exact path="/" auth={props.auth} component={Home} expenses={expenses} currentDate={currentDate} changeCurrentDate={changeCurrentDate} isLoading={isLoading} setExpenses={setExpenses} />} />
-        <ProtectedRoute exact path="/add-expense" auth={props.auth} component={!currentAccount._id ? Loader : isLoggedIn() && currentAccount.users.includes(localStorage.userName) ? AddExpense : Login} users={currentAccount.users} setCurrentUser={setCurrentUser} currentUser={currentUser} setExpenses={setExpenses} currentAccount={currentAccount}/>} />
+        <ProtectedRoute exact path="/" auth={props.auth} component={Home} expenses={expenses} currentDate={currentDate} changeCurrentDate={changeCurrentDate} isLoading={isLoading} setExpenses={setExpenses} />
+        <ProtectedRoute exact path="/add-expense" auth={props.auth} component={!currentAccount._id ? Loader : isLoggedIn() && currentAccount.users.includes(localStorage.userName) ? AddExpense : Login} users={currentAccount.users} setCurrentUser={setCurrentUser} currentUser={currentUser} setExpenses={setExpenses} currentAccount={currentAccount}/>
         <ProtectedRoute exact path="/reports" auth={props.auth} component={Reports} expenses={expenses} currentDate={currentDate} changeCurrentDate={changeCurrentDate} isLoading={isLoading} />
-        <ProtectedRoute exact path="/income" auth={props.auth} component={Income} currentDate={currentDate} changeCurrentDate={changeCurrentDate} isLoading={isLoading} />
+        <ProtectedRoute exact path="/income" auth={props.auth} component={Income} currentDate={currentDate} changeCurrentDate={changeCurrentDate} isLoading={isLoading} setIsLoading={setIsLoading} currentAccount={currentAccount} />
         <ProtectedRoute exact path='/settings' auth={props.auth} component={!currentAccount._id ? Loader : Settings} currentAccount={currentAccount} setCurrentAccount={setCurrentAccount} />
         <Route exact path='/signin' render={() => <AccountSignIn setCurrentAccount={setCurrentAccount} auth={props.auth} />} />
         <Route path="*" render={() => <NotFound />}/>
--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TableHeader from './general/TableHeader';
 import FabButton from './FabButton'
 import MonthSelector from './MonthSelector';
 import ExpensePanels from './ExpensePanels';
 import Loader from './Loader';
 import '../styles/Expense.css'
+import axios from 'axios'
+import { API_URL } from '../utils/utils';
 
 
 
 function Income(props) {
+    const { currentAccount, currentDate, setIsLoading, isLoading } = props
+    const [income, setIncome] = useState([])
+
+    useEffect(() => {
+        const getIncome = async (shouldGetByDate = true) => {
+            const optionalParam = shouldGetByDate ? `?date=${currentDate}` : ''
+
+            const res = await axios.get(`${API_URL}/api/income/${currentAccount._id}${optionalParam}`)
+            setIncome(res.data)
+            setIsLoading(false)
+        }
+
+        getIncome()
+    }, [])
 
     return (
         <div id='income-container'>
             <MonthSelector currentDate={props.currentDate} changeCurrentDate={props.changeCurrentDate} />
             <TableHeader type='הכנסה'/>
             {
-                props.expenses.length
-                    ? <ExpensePanels expenses={props.expenses} setExpenses={props.setExpenses} />
-                    : props.isLoading
+                income.length
+                    ? <ExpensePanels expenses={income} setExpenses={setIncome} />
+                    : isLoading
                         ? <Loader />
-                        : <div className="no-expense-msg">אין הוצאות לחודש זה</div>
+                        : <div className="no-expense-msg">אין הכנסות לחודש זה</div>
             }
             <FabButton />
         </div>
